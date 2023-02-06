137983b2965280e38f13e9987445e099ad65a81d
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-02-06 19:40:51 +0100

    propellor spin

---
+++ config.hs
@@ -0,0 +1,29 @@
+-- This is the main configuration file for Propellor, and is used to build
+-- the propellor program.    https://propellor.branchable.com/
+
+import Propellor
+import qualified Propellor.Property.File as File
+import qualified Propellor.Property.Apt as Apt
+import qualified Propellor.Property.Cron as Cron
+import qualified Propellor.Property.User as User
+
+main :: IO ()
+main = defaultMain hosts
+
+-- The hosts propellor knows about.
+hosts :: [Host]
+hosts =
+        [ mybox
+        ]
+
+-- An example host.
+mybox :: Host
+mybox = host "cardano.hydra.bzh" $ props
+        & osDebian Unstable X86_64
+        & Apt.stdSourcesList
+        & Apt.unattendedUpgrades
+        & Apt.installed ["etckeeper"]
+        & Apt.installed ["ssh"]
+        & User.hasSomePassword (User "root")
+        & File.dirExists "/var/www"
+        & Cron.runPropellor (Cron.Times "30 * * * *")
