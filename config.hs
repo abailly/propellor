98ec5b01b00fa3107d0e467735f2698efdf79ed7
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-02-07 03:01:52 +0100

    propellor spin

--- config.hs
+++ config.hs
@@ -10,7 +10,7 @@ import qualified Propellor.Property.Git as Git
 import qualified Propellor.Property.Ssh as Ssh
 import qualified Propellor.Property.User as User
 import Propellor.Types.MetaTypes (MetaType (..), MetaTypes)
-import Propellor.Utilities (doesFileExist, readProcess, doesDirectoryExist)
+import Propellor.Utilities (doesDirectoryExist, doesFileExist, readProcess)
 
 main :: IO ()
 main = defaultMain hosts
@@ -50,15 +50,19 @@ setupNode =
                 shouldDownload
                 ( cmdProperty
                     "curl"
-                    ["-o", "/home/curry/cardanode-node-1.35.5.tgz", "-L", "https://update-cardano-mainnet.iohk.io/cardano-node-releases/cardano-node-1.35.5-linux.tar.gz"]
-                    `changesFileContent` "/home/curry/cardanode-node-1.35.5.tgz"
+                    ["-o", "/home/curry/cardano-node-1.35.5.tgz", "-L", "https://update-cardano-mainnet.iohk.io/cardano-node-releases/cardano-node-1.35.5-linux.tar.gz"]
+                    `changesFileContent` "/home/curry/cardano-node-1.35.5.tgz"
                 )
-            & File.ownerGroup "/home/curry/cardanode-node-1.35.5.tgz" curry curryGrp
+            & File.ownerGroup "/home/curry/cardano-node-1.35.5.tgz" curry curryGrp
+            & cmdProperty
+                "tar"
+                ["xC", "/home/curry", "-f", "/home/curry/cardano-node-1.35.5.tgz"]
+                `changesFileContent` "/home/curry/cardano-node"
   where
     sha256 = "bb9e9c3700ebdef4de3e34e5087a79dc30d27ca3c1c66af25957f9205dfe05aa"
     shouldDownload = liftPropellor $ do
-        hasFile <- doesFileExist "/home/curry/cardanode-node-1.35.5.tgz"
-        sha <- head . words . head . lines <$> readProcess "sha256sum" ["/home/curry/cardanode-node-1.35.5.tgz"]
+        hasFile <- doesFileExist "/home/curry/cardano-node-1.35.5.tgz"
+        sha <- head . words . head . lines <$> readProcess "sha256sum" ["/home/curry/cardano-node-1.35.5.tgz"]
         pure $ not hasFile || sha /= sha256
 
     curry = User "curry"
