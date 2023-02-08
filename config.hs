70e2e4f18a67171b17b2b41d94da84b13c783cdb
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-02-08 08:24:45 +0100

    propellor spin

--- config.hs
+++ config.hs
@@ -31,7 +31,7 @@ cardano =
             & Apt.stdSourcesList
             & Apt.unattendedUpgrades
             & Apt.installed ["etckeeper"]
-            & Apt.installed ["ssh", "jq"]
+            & Apt.installed ["ssh", "jq", "tmux"]
             & User.hasSomePassword (User "root")
             & File.dirExists "/var/www"
             & Cron.runPropellor (Cron.Times "30 * * * *")
