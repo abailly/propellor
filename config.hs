c70af97fc949377b42990e51a757126a385b6ab7
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-05-28 22:52:05 +0200

    propellor spin

--- config.hs
+++ config.hs
@@ -63,7 +63,7 @@ clermont =
     installNix =
         [ "curl -o install-nix-2.15.0 https://releases.nixos.org/nix/nix-2.15.0/install"
         , "chmod +x ./install-nix-2.15.0"
-        , "./install-nix-2.15.0 --daemon"
+        , "./install-nix-2.15.0 --daemon < /dev/null"
         ]
 
 cardano :: Host
