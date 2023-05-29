fe9e6ad742713ad48b3e5e07e223b034747b9560
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-05-29 09:57:29 +0200

    propellor spin

--- config.hs
+++ config.hs
@@ -3,20 +3,20 @@
 {-# LANGUAGE DataKinds #-}
 {-# OPTIONS_GHC -Wno-deferred-out-of-scope-variables #-}
 
+import Base (OS)
+import Cardano (setupNode)
 import Propellor
 import qualified Propellor.Property.Apt as Apt
 import qualified Propellor.Property.Cron as Cron
 import qualified Propellor.Property.File as File
 import Propellor.Property.Firewall (Chain (..), ConnectionState (..), Proto (..), Rules (..), Table (..), Target (..), rule)
 import qualified Propellor.Property.Firewall as Firewall
-import qualified Propellor.Property.Git as Git
 import qualified Propellor.Property.Ssh as Ssh
 import qualified Propellor.Property.Systemd as Systemd
 import qualified Propellor.Property.Tor as Tor
 import qualified Propellor.Property.User as User
 import Propellor.Types.MetaTypes (MetaType (..), MetaTypes)
-import Propellor.Utilities (doesDirectoryExist, doesFileExist, readProcess, readProcessEnv)
-import System.FilePath ((</>))
+import Propellor.Utilities (readProcess)
 
 main :: IO ()
 main = defaultMain hosts
@@ -33,16 +33,17 @@ clermont =
             & Apt.stdSourcesList
             & Apt.unattendedUpgrades
             & Apt.installed ["etckeeper"]
-            & Apt.installed ["ssh", "jq", "tmux", "dstat", "git"]
+            & Apt.installed ["ssh", "jq", "tmux", "dstat", "git", "emacs-nox"]
             & check shouldInstallNix (scriptProperty installNix)
             & File.hasContent "/etc/nix/nix.conf" nixConf
+            & Systemd.started "nix-daemon.service"
             & Ssh.installed
             & Systemd.persistentJournal
             & User.accountFor user
             & User.hasGroup user nixGrp
             & Ssh.authorizedKey user ""
             & Ssh.authorizedKeys user hostContext
-            & setupNode
+            & setupNode user
   where
     user = User "curry"
     nixGrp = Group "nixbld"
@@ -83,10 +84,10 @@ cardano =
             & Cron.runPropellor (Cron.Times "30 * * * *")
             & Systemd.persistentJournal
             & firewall
-            & setupNode
+            & setupNode user
             & setupHydraNode
-
-type OS = MetaTypes '[ 'WithInfo, 'Targeting 'OSDebian, 'Targeting 'OSBuntish]
+  where
+    user = User "curry"
 
 firewall :: Property OS
 firewall =
@@ -139,119 +140,6 @@ setupHydraNode =
         , "[Install]"
         , "WantedBy=multi-user.target"
         ]
-
-setupNode :: Property OS
-setupNode =
-    propertyList "Cardano node" $
-        props
-            & User.accountFor user
-            & Ssh.authorizedKeys user hostContext
-            & check
-                ( do
-                    d <- User.homedir user
-                    not <$> doesDirectoryExist (d </> "cardano-configurations")
-                )
-                (Git.pulled user "https://github.com/input-output-hk/cardano-configurations" "cardano-configurations" Nothing)
-            & check
-                shouldDownload
-                ( cmdProperty
-                    "curl"
-                    ["-o", archivePath, "-L", "https://update-cardano-mainnet.iohk.io/cardano-node-releases/cardano-node-8.0.0-linux.tar.gz"]
-                    `changesFileContent` archivePath
-                )
-            & File.ownerGroup archivePath user userGrp
-            & check
-                shouldUnpack
-                ( cmdProperty
-                    "tar"
-                    ["xC", "/home/curry", "-f", archivePath]
-                    `changesFileContent` "/home/curry/cardano-node"
-                )
-            & generateTopologyFile
-            & File.hasContent "/home/curry/cardano-node.environment" envFile
-            & File.hasContent "/etc/systemd/system/cardano-node.service" serviceNode
-            & Systemd.enabled "cardano-node"
-            & Systemd.started "cardano-node"
-  where
-    sha256 = "478fb9a9b1f214b22fc076f9c7db93c4b0dd38f1700400eb8ca44fe9e4e7a011"
-
-    archivePath = "/home/curry/cardano-node-8.0.0.tgz"
-
-    shouldUnpack =
-        liftPropellor $ do
-            dir <- User.homedir user
-            hasFile <- doesFileExist (dir </> "cardano-node")
-            if not hasFile
-                then pure True
-                else not . ("8.0.0" `elem`) . words . head . lines <$> readProcessEnv "/home/curry/cardano-node" ["--version"] (Just [("LD_LIBRARY_PATH", dir)])
-
-    shouldDownload = liftPropellor $ do
-        hasFile <- doesFileExist archivePath
-        if not hasFile
-            then pure True
-            else (/= sha256) . head . words . head . lines <$> readProcess "sha256sum" [archivePath]
-
-    user = User "curry"
-
-    userGrp = Group "curry"
-
-    envFile =
-        [ "CONFIG=\"/home/curry/cardano-configurations/network/mainnet/cardano-node/config.json\""
-        , "TOPOLOGY=\"/home/curry/topology.json\""
-        , "DBPATH=\"./db/\""
-        , "SOCKETPATH=\"./node.socket\""
-        , "HOSTADDR=\"0.0.0.0\""
-        , "PORT=\"3001\""
-        , "LD_LIBRARY_PATH=\"/home/curry\""
-        ]
-
-    serviceNode =
-        [ "[Unit]"
-        , "Description=Cardano node"
-        , "After=multi-user.target"
-        , ""
-        , "[Service]"
-        , "Type=simple"
-        , "EnvironmentFile=/home/curry/cardano-node.environment"
-        , "ExecStart=/home/curry/cardano-node run --config $CONFIG --topology $TOPOLOGY --database-path $DBPATH --socket-path $SOCKETPATH --host-addr $HOSTADDR --port $PORT"
-        , "KillSignal = SIGINT"
-        , "RestartKillSignal = SIGINT"
-        , "StandardOutput=journal"
-        , "StandardError=journal"
-        , "SyslogIdentifier=cardano-node"
-        , ""
-        , "LimitNOFILE=32768"
-        , ""
-        , "Restart=on-failure"
-        , "RestartSec=15s"
-        , "StartLimitIntervalSec=0"
-        , "WorkingDirectory=~"
-        , "User=curry"
-        , "Group=curry"
-        , ""
-        , "[Install]"
-        , "WantedBy=multi-user.target"
-        ]
-
-    generateTopologyFile =
-        propertyList "Random topology.json" $
-            props
-                & Apt.installed ["jq", "curl", "coreutils"]
-                & check
-                    (not <$> doesFileExist "/home/curry/topology.json")
-                    (scriptProperty [randomPeers])
-                & File.ownerGroup "/home/curry/topology.json" user userGrp
-
-    randomPeers =
-        concat
-            [ "curl https://explorer.mainnet.cardano.org/relays/topology.json | "
-            , "jq -rc '(.Producers[] | "
-            , "{addr:.addr,port:.port,valency:1})' | "
-            , "shuf | "
-            , "head -20 | "
-            , "jq -s '(. | {Producers:.})' > "
-            , "/home/curry/topology.json"
-            ]
 
 {- |A basic rule to drop every input packet
 
