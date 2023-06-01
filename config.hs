4b7ba31fbc081eaa6c9cefb131a589714f301f0b
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-06-01 08:55:57 +0200

    propellor spin

--- config.hs
+++ config.hs
@@ -39,7 +39,7 @@ clermont =
             & Apt.stdSourcesList
             & Apt.unattendedUpgrades
             & Apt.installed ["etckeeper"]
-            & Apt.installed ["ssh", "jq", "tmux", "dstat", "git", "emacs-nox", "silversearcher-ag"]
+            & Apt.installed ["ssh", "jq", "tmux", "dstat", "git", "emacs-nox", "silversearcher-ag", "direnv"]
             & installNix
             & File.hasContent "/etc/nix/nix.conf" nixConf
             & Systemd.started "nix-daemon.service"
