b425b4bd38927e32ab9d8b6383d054bb4e2853b5
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-02-03 14:20:41 +0100

    propellor spin

--- Hydra.hs
+++ Hydra.hs
@@ -3,53 +3,55 @@
 module Hydra where
 
 import Cardano (shouldDownload)
+import Data.List (isInfixOf)
 import Propellor
-import Propellor.Base (combineModes, doesFileExist, readProcessEnv, (<.>), (</>))
+import Propellor.Base (combineModes, doesFileExist, liftIO, readProcessEnv, (<.>), (</>))
 import qualified Propellor.Property.Apt as Apt
 import qualified Propellor.Property.File as File
 import qualified Propellor.Property.Systemd as Systemd
 import qualified Propellor.Property.User as User
 import Propellor.Types.MetaTypes (MetaType (..), MetaTypes)
 import System.Posix (ownerExecuteMode, ownerReadMode, ownerWriteMode)
-import Data.List (isInfixOf)
-import User (commonUserSetup)
 
 setup :: User -> Property (MetaTypes '[ 'Targeting 'OSDebian, 'Targeting 'OSBuntish])
 setup user =
-    propertyList "Hydra node" $
-        props
-            & check
-                (shouldDownload sha256 ("/home/curry" </> archivePath))
-                ( userScriptProperty
-                    user
-                    ["curl -o " <> archivePath <> " -L " <> hydraNodeArchiveUrl]
-                    `changesFileContent` archivePath
-                )
-                `describe` ("Hydra node " <> hydraVersion <> " archive downloaded")
-            & check
-                shouldUnpack
-                ( userScriptProperty
-                    user
-                    ["unzip -qo " <> archivePath]
-                    `changesFileContent` "/home/curry/hydra-node"
-                    `requires` Apt.installed ["unzip"]
-                )
-                `describe` ("Hydra node " <> hydraVersion <> " archive unpacked")
-            & File.mode "/home/curry/hydra-node" (combineModes [ownerReadMode, ownerWriteMode, ownerExecuteMode])
-            & File.hasContent "/home/curry/hydra-node.environment" envFile
-            & File.hasContent "/home/curry/run-hydra.sh" hydraRunFile
-              `requires` Systemd.stopped "hydra-node"
-            & File.mode "/home/curry/run-hydra.sh" (combineModes [ownerReadMode, ownerWriteMode, ownerExecuteMode])
-            & File.ownerGroup "/home/curry/run-hydra.sh" user userGrp
-            & File.hasContent "/etc/systemd/system/hydra-node.service" serviceFile
-            & Systemd.enabled "hydra-node"
-            & Systemd.restarted "hydra-node"
+    property' ("Hydra Node for " <> show user) $ \witness -> do
+        home <- liftIO $ User.homedir user
+        ensureProperty witness $
+            propertyList "Hydra node" $
+                props
+                    & check
+                        (shouldDownload sha256 (home </> archivePath))
+                        ( userScriptProperty
+                            user
+                            ["curl -o " <> archivePath <> " -L " <> hydraNodeArchiveUrl]
+                            `changesFileContent` archivePath
+                        )
+                        `describe` ("Hydra node " <> hydraVersion <> " archive downloaded")
+                    & check
+                        shouldUnpack
+                        ( userScriptProperty
+                            user
+                            ["unzip -qo " <> archivePath]
+                            `changesFileContent` (home </> "hydra-node")
+                            `requires` Apt.installed ["unzip"]
+                        )
+                        `describe` ("Hydra node " <> hydraVersion <> " archive unpacked")
+                    & File.mode (home </> "hydra-node") (combineModes [ownerReadMode, ownerWriteMode, ownerExecuteMode])
+                    & File.hasContent (home </> "hydra-node.environment") (envFile home)
+                    & File.hasContent (home </> "run-hydra.sh") hydraRunFile
+                        `requires` Systemd.stopped "hydra-node"
+                    & File.mode (home </> "run-hydra.sh") (combineModes [ownerReadMode, ownerWriteMode, ownerExecuteMode])
+                    & File.ownerGroup (home </> "run-hydra.sh") user userGrp
+                    & File.hasContent "/etc/systemd/system/hydra-node.service" (serviceFile home)
+                    & Systemd.enabled "hydra-node"
+                    & Systemd.restarted "hydra-node"
   where
     hydraNodeArchiveUrl =
         "https://github.com/input-output-hk/hydra/releases/download"
             </> hydraVersion
             </> "hydra-x86_64-linux-"
-            <> hydraVersion <.> "zip"
+                <> hydraVersion <.> "zip"
 
     sha256 = "760dbc71dfb01501003f80d6fd768ba9734202c908a70d39cb01b99f5abe5dc7"
 
@@ -71,20 +73,20 @@ setup user =
 
     userGrp = Group "curry"
 
-    envFile =
-        [ "SOCKETPATH=/home/curry/node.socket"
+    envFile home =
+        [ "SOCKETPATH=" <> home </> "node.socket"
         , "HYDRA_SCRIPTS_TX_ID=c21b70a719112f76d318abcfee95499a27556ef4a18b1c62bc38e4b9c07ae3a6"
         ]
 
-    serviceFile =
+    serviceFile home =
         [ "[Unit]"
         , "Description=Hydra node"
         , "After=multi-user.target"
         , ""
         , "[Service]"
         , "Type=simple"
-        , "EnvironmentFile=/home/curry/hydra-node.environment"
-        , "ExecStart=/home/curry/run-hydra.sh"
+        , "EnvironmentFile=" <> home </> "hydra-node.environment"
+        , "ExecStart=" <> home </> "run-hydra.sh"
         , "KillSignal = SIGINT"
         , "RestartKillSignal = SIGINT"
         , "StandardOutput=journal"
@@ -104,40 +106,40 @@ setup user =
         , "WantedBy=multi-user.target"
         ]
 
-    hydraRunFile = [
-             "#!/usr/bin/env bash"
-            , ""
-            , "set -vxe"
-            , ""
-            , ". hydra-node.environment"
-            , ""
-            , "./hydra-node \\"
-            , "  --node-id  arnaud@preview \\"
-            , "  --api-host   0.0.0.0 \\"
-            , "  --host  0.0.0.0 \\"
-            , "  --port  5001 \\"
-            , "  --monitoring-port  6001 \\"
-            , "  --persistence-dir  hydra-data \\"
-            , "  --hydra-signing-key  keys/arnaud-hydra.sk \\"
-            , "  --cardano-signing-key  keys/arnaud.sk \\"
-            , "  --ledger-protocol-parameters  protocol-parameters.json \\"
-            , "  --testnet-magic 2 \\"
-            , "  --hydra-scripts-tx-id  ${HYDRA_SCRIPTS_TX_ID} \\"
-            , "  --node-socket ${SOCKETPATH} \\"
-            -- sebastian
-            , "  --peer  fk.ncoding.at:5002 \\"
-            , "  --cardano-verification-key  keys/sebastian.cardano.vk \\"
-            , "  --hydra-verification-key  keys/sebastian.hydra.vk \\"
-            -- sasha
-            , "  --peer 13.37.150.125:5001 \\"
-            , "  --cardano-verification-key keys/sasha.cardano.vk \\"
-            , "  --hydra-verification-key keys/sasha.hydra.vk  \\"
-            -- franco
-            , "  --peer 13.39.83.131:5001 \\"
-            , "  --cardano-verification-key keys/franco.cardano.vk \\"
-            , "  --hydra-verification-key keys/franco.hydra.vk \\"
-            -- daniel
-            , "  --peer hydra.horizon-haskell.net:5005 \\"
-            , "  --cardano-verification-key keys/daniel.cardano.vk \\"
-            , "  --hydra-verification-key keys/daniel.hydra.vk"
-       ]
+    hydraRunFile =
+        [ "#!/usr/bin/env bash"
+        , ""
+        , "set -vxe"
+        , ""
+        , ". hydra-node.environment"
+        , ""
+        , "./hydra-node \\"
+        , "  --node-id  arnaud@preview \\"
+        , "  --api-host   0.0.0.0 \\"
+        , "  --host  0.0.0.0 \\"
+        , "  --port  5001 \\"
+        , "  --monitoring-port  6001 \\"
+        , "  --persistence-dir  hydra-data \\"
+        , "  --hydra-signing-key  keys/arnaud-hydra.sk \\"
+        , "  --cardano-signing-key  keys/arnaud.sk \\"
+        , "  --ledger-protocol-parameters  protocol-parameters.json \\"
+        , "  --testnet-magic 2 \\"
+        , "  --hydra-scripts-tx-id  ${HYDRA_SCRIPTS_TX_ID} \\"
+        , "  --node-socket ${SOCKETPATH} \\"
+        , -- sebastian
+          "  --peer  fk.ncoding.at:5002 \\"
+        , "  --cardano-verification-key  keys/sebastian.cardano.vk \\"
+        , "  --hydra-verification-key  keys/sebastian.hydra.vk \\"
+        , -- sasha
+          "  --peer 13.37.150.125:5001 \\"
+        , "  --cardano-verification-key keys/sasha.cardano.vk \\"
+        , "  --hydra-verification-key keys/sasha.hydra.vk  \\"
+        , -- franco
+          "  --peer 13.39.83.131:5001 \\"
+        , "  --cardano-verification-key keys/franco.cardano.vk \\"
+        , "  --hydra-verification-key keys/franco.hydra.vk \\"
+        , -- daniel
+          "  --peer hydra.horizon-haskell.net:5005 \\"
+        , "  --cardano-verification-key keys/daniel.cardano.vk \\"
+        , "  --hydra-verification-key keys/daniel.hydra.vk"
+        ]
