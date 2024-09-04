f507a24cf786be11be2e1f6005cf712ddb7c083c
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-09-04 10:35:49 +0200

    propellor spin

--- Radicle.hs
+++ Radicle.hs
@@ -16,8 +16,8 @@ seedInstalled userName =
         propertyList "Radicle seed installed" $
             props
                 & User.systemAccountFor' user (Just "/home/seed") (Just group)
-                & downloadAndInstall (Package "radicle" radicleKey radicleUrl radicleSigUrl radicleSHA256Url)
-                & downloadAndInstall (Package "radicle-http" radicleHttpKey radicleHttpUrl radicleHttpSigUrl radicleHttpSHA256Url)
+                & downloadAndInstall (Package "radicle" radicleKey radicleUrl radicleSigUrl radicleSHA256Url radicleVersion)
+                & downloadAndInstall (Package "radicle-http" radicleHttpKey radicleHttpUrl radicleHttpSigUrl radicleHttpSHA256Url radicleHttpVersion)
 
     teardownRadicleSeed =
         tightenTargets $
@@ -28,17 +28,22 @@ seedInstalled userName =
     user = User userName
     group = Group userName
 
+    -- FIXME: should not be hardcoded
+    home = "/home" </> userName
+
+    radicleVersion = "1.0.0-rc.17"
     radicleKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL460KIEccS4881p7PPpiiQBsxF+H5tgC6De6crw9rbU"
     radicleUrl = "https://files.radicle.xyz/releases/latest/radicle-1.0.0-rc.17-x86_64-unknown-linux-musl.tar.xz"
     radicleSigUrl = "https://files.radicle.xyz/releases/latest/radicle-1.0.0-rc.17-x86_64-unknown-linux-musl.tar.xz.sig"
     radicleSHA256Url = "https://files.radicle.xyz/releases/latest/radicle-1.0.0-rc.17-x86_64-unknown-linux-musl.tar.xz.sha256"
 
+    radicleHttpVersion = "0.17.0"
     radicleHttpKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKU7IHRsae2q1/qd8NaWxfGhPEFGHwK1dcxvSjNdttjb"
     radicleHttpUrl = "https://files.radicle.xyz/releases/radicle-httpd/latest/radicle-httpd-0.17.0-x86_64-unknown-linux-musl.tar.xz"
     radicleHttpSigUrl = "https://files.radicle.xyz/releases/radicle-httpd/latest/radicle-httpd-0.17.0-x86_64-unknown-linux-musl.tar.xz.sig"
     radicleHttpSHA256Url = "https://files.radicle.xyz/releases/radicle-httpd/latest/radicle-httpd-0.17.0-x86_64-unknown-linux-musl.tar.xz.sha256"
 
-    downloadAndInstall Package{name, url, sha256Url} =
+    downloadAndInstall Package{name, url, sha256Url, version} =
         propertyList ("Download and install " <> name) $
             props
                 & check
@@ -53,17 +58,16 @@ seedInstalled userName =
                                     <> " downloaded"
                                )
                 & File.ownerGroup archivePath user group
+                & check
+                    (shouldUnpack name version)
+                    ( cmdProperty
+                        "tar"
+                        ["xC", home, "-f", archivePath]
+                        `changesFileContent` ("home" </> "bin" </> "cardano-node")
+                    )
+                    `describe` "Cardano node 9.1.1 archive unpacked"
       where
         archivePath = "/home" </> userName </> name <.> "tar.xz"
-
-    -- & check
-    --     shouldUnpack
-    --     ( cmdProperty
-    --         "tar"
-    --         ["xC", home, "-f", archivePath]
-    --         `changesFileContent` ("home" </> "bin" </> "cardano-node")
-    --     )
-    --     `describe` "Cardano node 9.1.1 archive unpacked"
 
     shouldDownload sha256Url archivePath = do
         sha256 <- head . words . head . lines <$> readProcess "curl" ["-o", "-", "-L", sha256Url]
@@ -72,12 +76,26 @@ seedInstalled userName =
             then pure True
             else (/= sha256) . head . words . head . lines <$> readProcess "/usr/bin/sha256sum" [archivePath]
 
+    shouldUnpack name version = do
+        let binFile = home </> ".radicle" </> "bin" </> name
+        hasFile <- doesFileExist binFile
+        if hasFile
+            then
+                not
+                    . (radicleVersion `elem`)
+                    . words
+                    . head
+                    . lines
+                    <$> readProcess binFile ["--version"]
+            else pure True
+
 data Package = Package
     { name :: String
     , key :: String
     , url :: String
     , sigUrl :: String
     , sha256Url :: String
+    , version :: String
     }
 
 -- downloadAndInstall :: String -> Package -> Property
