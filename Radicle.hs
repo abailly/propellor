44499bdafb513af99d91c6588e28bee51f118c00
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-09-08 11:13:07 +0200

    propellor spin

--- Radicle.hs
+++ Radicle.hs
@@ -78,7 +78,7 @@ radicleSeedInstalled =
                         usrLocal
                         (archivePath "radicle-http")
                         radicleHttpPackage
-                    & configureRadicle user
+                    & authenticateRadicle user "/usr/local"
                     & serviceConfigured user
                     & Systemd.enabled "radicle-node"
                     & Systemd.restarted "radicle-node"
@@ -133,23 +133,9 @@ serviceConfigured user@(User userName) =
     tightenTargets $
         propertyList "Radicle node configured" $
             props
-                & configFileForNode
-                & File.ownerGroup configFilePath user group
+                & configFileForNode user configFile
                 & serviceFileForNode
   where
-    group = Group userName
-
-    configFilePath = "/home" </> userName </> ".radicle" </> "config.json"
-
-    configFileForNode :: Property OS
-    configFileForNode =
-        property' "radicle node config file" $ \w -> do
-            host <- asks hostName
-            ensureProperty w $
-                ( File.hasContent configFilePath (configFile host)
-                    <> File.ownerGroup configFilePath user group
-                )
-
     serviceFileForNode :: Property OS
     serviceFileForNode =
         withPrivData (PrivFile "radicle-pwd") hostContext $ \getPrivDataPwd ->
@@ -196,6 +182,20 @@ serviceConfigured user@(User userName) =
         , "WantedBy=multi-user.target"
         ]
 
+configFileForNode :: User -> (String -> [String]) -> Property OS
+configFileForNode user@(User userName) configuration =
+    property' "radicle node config file" $ \w -> do
+        dir <- liftIO $ User.homedir user
+        host <- asks hostName
+        ensureProperty w $
+            ( File.hasContent (configFilePath dir) (configuration host)
+                <> File.ownerGroup (configFilePath dir) user group
+            )
+  where
+    group = Group userName
+
+    configFilePath dir = dir </> ".radicle" </> "config.json"
+
 radicleInstalledFor :: User -> RevertableProperty OS OS
 radicleInstalledFor user@(User userName) =
     setupRadicle <!> teardownRadicle
@@ -211,7 +211,8 @@ radicleInstalledFor user@(User userName) =
                     radicleDir
                     (archivePath "radicle")
                     radiclePackage
-                & configureRadicle user
+                & authenticateRadicle user radicleDir
+                & configFileForNode user configFile
                 & nodeRunning user (radicleDir </> "bin" </> "rad")
 
     teardownRadicle =
@@ -242,6 +243,66 @@ radicleInstalledFor user@(User userName) =
 
     radicleDir = home </> ".radicle"
     archivePath name = home </> name <.> "tar.xz"
+
+    configFile host =
+        [ "{"
+        , "  \"publicExplorer\": \"https://app.radicle.xyz/nodes/$host/$rid$path\","
+        , "  \"preferredSeeds\": ["
+        , "    \"z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@seed.radicle.garden:8776\","
+        , "    \"z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@ash.radicle.garden:8776\","
+        , "    \"z6MkfiRENtzUJiU1kxLhxWMWFCiGGxGi6jEbj33Pq9zBVQkK@cardano.hydra.bzh:8776\""
+        , "  ],"
+        , "  \"web\": {"
+        , "    \"pinned\": {"
+        , "      \"repositories\": []"
+        , "    }"
+        , "  },"
+        , "  \"cli\": {"
+        , "    \"hints\": true"
+        , "  },"
+        , "  \"node\": {"
+        , "    \"alias\": \"" <> userName <.> host <> "\","
+        , "    \"listen\": [\"0.0.0.0:8776\"],"
+        , "    \"onion\": {"
+        , "        \"mode\": \"proxy\","
+        , "        \"address\": \"127.0.0.1:9050\""
+        , "    },"
+        , "    \"peers\": {"
+        , "      \"type\": \"dynamic\""
+        , "    },"
+        , "    \"connect\": [],"
+        , "    \"externalAddresses\": [],"
+        , "    \"network\": \"main\","
+        , "    \"log\": \"INFO\","
+        , "    \"relay\": \"auto\","
+        , "    \"limits\": {"
+        , "      \"routingMaxSize\": 1000,"
+        , "      \"routingMaxAge\": 604800,"
+        , "      \"gossipMaxAge\": 1209600,"
+        , "      \"fetchConcurrency\": 1,"
+        , "      \"maxOpenFiles\": 4096,"
+        , "      \"rate\": {"
+        , "        \"inbound\": {"
+        , "          \"fillRate\": 5.0,"
+        , "          \"capacity\": 1024"
+        , "        },"
+        , "        \"outbound\": {"
+        , "          \"fillRate\": 10.0,"
+        , "          \"capacity\": 2048"
+        , "        }"
+        , "      },"
+        , "      \"connection\": {"
+        , "        \"inbound\": 128,"
+        , "        \"outbound\": 16"
+        , "      }"
+        , "    },"
+        , "    \"workers\": 8,"
+        , "    \"seedingPolicy\": {"
+        , "      \"default\": \"block\""
+        , "    }"
+        , "  }"
+        , "}"
+        ]
 
 downloadAndInstall ::
     User ->
@@ -310,89 +371,27 @@ nodeRunning user radExe =
                         `assume` NoChange
                     )
 
-configureRadicle :: User -> Property OS
-configureRadicle user@(User userName) =
+authenticateRadicle :: User -> FilePath -> Property OS
+authenticateRadicle user@(User userName) exePath =
     withPrivData (PrivFile "radicle-seed") hostContext $ \getPrivDataSeed ->
         withPrivData (PrivFile "radicle-pwd") hostContext $ \getPrivDataPwd ->
             property' "radicle configured" $ \w -> do
                 host <- asks hostName
                 getPrivDataSeed $ \(PrivData privDataSeed) ->
                     getPrivDataPwd $ \(PrivData privDataPwd) -> do
-                        dir <- liftIO $ User.homedir user
                         ensureProperty
                             w
-                            ( radAuth user (userName <.> host) privDataSeed privDataPwd
-                                <> File.hasContent (dir </> ".radicle" </> "config.json") (radicleConfig host)
+                            ( radAuth exePath user (userName <.> host) privDataSeed privDataPwd
                             )
-  where
-    radicleConfig host =
-        [ "{"
-        , "  \"publicExplorer\": \"https://app.radicle.xyz/nodes/$host/$rid$path\","
-        , "  \"preferredSeeds\": ["
-        , "    \"z6MkrLMMsiPWUcNPHcRajuMi9mDfYckSoJyPwwnknocNYPm7@seed.radicle.garden:8776\","
-        , "    \"z6Mkmqogy2qEM2ummccUthFEaaHvyYmYBYh3dbe9W4ebScxo@ash.radicle.garden:8776\","
-        , "    \"z6MkfiRENtzUJiU1kxLhxWMWFCiGGxGi6jEbj33Pq9zBVQkK@cardano.hydra.bzh:8776\""
-        , "  ],"
-        , "  \"web\": {"
-        , "    \"pinned\": {"
-        , "      \"repositories\": []"
-        , "    }"
-        , "  },"
-        , "  \"cli\": {"
-        , "    \"hints\": true"
-        , "  },"
-        , "  \"node\": {"
-        , "    \"alias\": \"" <> userName <.> host <> "\","
-        , "    \"listen\": [\"0.0.0.0:8776\"],"
-        , "    \"onion\": {"
-        , "        \"mode\": \"proxy\","
-        , "        \"address\": \"127.0.0.1:9050\""
-        , "    },"
-        , "    \"peers\": {"
-        , "      \"type\": \"dynamic\""
-        , "    },"
-        , "    \"connect\": [],"
-        , "    \"externalAddresses\": [],"
-        , "    \"network\": \"main\","
-        , "    \"log\": \"INFO\","
-        , "    \"relay\": \"auto\","
-        , "    \"limits\": {"
-        , "      \"routingMaxSize\": 1000,"
-        , "      \"routingMaxAge\": 604800,"
-        , "      \"gossipMaxAge\": 1209600,"
-        , "      \"fetchConcurrency\": 1,"
-        , "      \"maxOpenFiles\": 4096,"
-        , "      \"rate\": {"
-        , "        \"inbound\": {"
-        , "          \"fillRate\": 5.0,"
-        , "          \"capacity\": 1024"
-        , "        },"
-        , "        \"outbound\": {"
-        , "          \"fillRate\": 10.0,"
-        , "          \"capacity\": 2048"
-        , "        }"
-        , "      },"
-        , "      \"connection\": {"
-        , "        \"inbound\": 128,"
-        , "        \"outbound\": 16"
-        , "      }"
-        , "    },"
-        , "    \"workers\": 8,"
-        , "    \"seedingPolicy\": {"
-        , "      \"default\": \"block\""
-        , "    }"
-        , "  }"
-        , "}"
-        ]
 
-radAuth :: User -> String -> String -> String -> Property UnixLike
-radAuth user nodeName privDataSeed privDataPwd =
+radAuth :: FilePath -> User -> String -> String -> String -> Property UnixLike
+radAuth exePath user nodeName privDataSeed privDataPwd =
     check (not <$> keysExist) $
         userScriptPropertyPty
             user
             [ "export RAD_KEYGEN_SEED=" <> privDataSeed
             , "export RAD_PASSPHRASE=" <> privDataPwd
-            , "rad auth --alias " <> nodeName
+            , exePath </> "bin" </> "rad auth --alias " <> nodeName
             ]
   where
     keysExist = do
