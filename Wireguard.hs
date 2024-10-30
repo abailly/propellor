7b37d9785ade3c03e78eef8e6ea858b36d9f2208
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-10-30 06:55:11 +0100

    propellor spin

--- Wireguard.hs
+++ Wireguard.hs
@@ -1,21 +1,68 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Wireguard where
 
-import Base (OSNoInfo)
-import Propellor (Property, RevertableProperty, propertyList, props, (&), (<!>))
+import Base (OS)
+import Data.String (IsString)
+import Propellor (
+  PrivData (..),
+  PrivDataField (..),
+  Property,
+  RevertableProperty,
+  assume,
+  ensureProperty,
+  hostContext,
+  property',
+  propertyList,
+  props,
+  requires,
+  scriptProperty,
+  tightenTargets,
+  (&),
+  (<!>),
+ )
+import Propellor.Base (Result (MadeChange), withPrivData)
 import qualified Propellor.Property.Apt as Apt
+import qualified Propellor.Property.File as File
 
-installed :: RevertableProperty OSNoInfo OSNoInfo
+newtype IFace = IFace String
+  deriving newtype (Show, Eq, IsString)
+
+installed :: RevertableProperty OS OS
 installed =
   setupWireguard <!> teardownWireguard
  where
-  setupWireguard :: Property OSNoInfo
+  setupWireguard :: Property OS
   setupWireguard =
     propertyList "Wireguard installed" $
       props
         & Apt.installed ["wireguard"]
+        & interfaceConfigured wg0
 
-  teardownWireguard :: Property OSNoInfo
+  wg0 = IFace "wg0"
+
+  interfaceConfigured :: IFace -> Property OS
+  interfaceConfigured (IFace ifName) =
+    withPrivData (PrivFile "wireguard-key") hostContext $ \getPrivData ->
+      property' ("wireguard interface " <> ifName) $ \w ->
+        getPrivData $ \(PrivData privateKey) ->
+          ensureProperty w $
+            scriptProperty ["wg-quick up " <> ifName]
+              `assume` MadeChange
+              `requires` File.hasContent ("/etc/wireguard/" <> ifName <> ".conf") (configFile privateKey)
+
+  teardownWireguard :: Property OS
   teardownWireguard =
-    propertyList "Wireguard removed" $
-      props
-        & Apt.removed ["wireguard"]
+    tightenTargets $
+      propertyList "Wireguard removed" $
+        props
+          & Apt.removed ["wireguard"]
+
+  configFile privateKey =
+    [ "[Interface]"
+    , "Address = 10.10.0.1/16"
+    , "SaveConfig = true"
+    , "PrivateKey = " <> privateKey
+    , "ListenPort = 51820"
+    ]
