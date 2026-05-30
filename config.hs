c4d272ed6fff5beee4e21cf9441f0b165ab16736
Arnaud Bailly <arnaud@pankzsoft.com>
2026-05-30 14:02:58 +0200

    propellor spin

--- config.hs
+++ config.hs
@@ -644,7 +644,7 @@ cardano =
       & Apt.unattendedUpgrades
       & Apt.update `onChange` Apt.safeUpgrade
       & Apt.installed ["etckeeper"]
-      & Apt.installed ["ssh", "jq", "tmux", "dstat", "libssl-dev"]
+      & Apt.installed ["ssh", "jq", "tmux", "dstat", "openssl-dev"]
       & Apt.autoRemove
       & Ssh.installed
       & Tor.installed
